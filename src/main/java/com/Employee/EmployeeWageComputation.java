3c7756430dba98f4f03ab49a032d93a4018e5658
kiran12kuhrade <Github@1999>
2024-09-26 14:41:31 +0530

    Added:CheckEmployeePresentee

--- src/main/java/com/Employee/EmployeeWageComputation.java
+++ src/main/java/com/Employee/EmployeeWageComputation.java
@@ -4,5 +4,20 @@ public class EmployeeWageComputation {
     public static void main(String[] args) {
 
         System.out.println("Welcome to Employee Wage Computtaion!!");
+
+        final int FULL_TIME = 1;
+
+        double empType = Math.floor(Math.random() * 10) % 2;
+        if (empType == FULL_TIME)
+        {
+            System.out.println("Employee is Present");
+
+
+        } else
+        {
+            System.out.println("Employee is Absent");
+
+
+        }
     }
 }

096f74df60253f4af4eec5302787066dcc4bc09e
kiran12kuhrade <Github@1999>
2024-09-26 14:47:34 +0530

    Added:Calculation of Daily Wage

--- src/main/java/com/Employee/EmployeeWageComputation.java
+++ src/main/java/com/Employee/EmployeeWageComputation.java
@@ -6,18 +6,21 @@ public class EmployeeWageComputation {
         System.out.println("Welcome to Employee Wage Computtaion!!");
 
         final int FULL_TIME = 1;
+        int WAGE_PER_HR = 20;
+        int workingHours = 0;
 
         double empType = Math.floor(Math.random() * 10) % 2;
         if (empType == FULL_TIME)
         {
             System.out.println("Employee is Present");
-
+            workingHours = 8;
 
         } else
         {
             System.out.println("Employee is Absent");
-
-
         }
+
+        int wage = workingHours * WAGE_PER_HR;
+        System.out.println("Employee Daily Wage is " + wage);
     }
 }

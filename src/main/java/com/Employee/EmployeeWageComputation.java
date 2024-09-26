55aa5a5c8c41e71c44cdac6ae5157c5a9b4047b3
kiran12kuhrade <Github@1999>
2024-09-26 15:18:49 +0530

    Added:CalculationWorkingWagePerMonth

--- src/main/java/com/Employee/EmployeeWageComputation.java
+++ src/main/java/com/Employee/EmployeeWageComputation.java
@@ -5,28 +5,32 @@ public class EmployeeWageComputation {
 
         System.out.println("Welcome to Employee Wage Computtaion!!");
 
-        final int FULL_TIME = 2;
         final int PART_TIME = 1;
+        final int FULL_TIME = 2;
         final int WAGE_PER_HR = 20;
-        final int WORKING_DAYS = 30;
+        final int MAX_WORKING_DAYS = 20;
+        final int MAX_WORKING_HRS = 100;
         int totalWage = 0;
-        for (int day = 1; day <= WORKING_DAYS; day++)
+        int workingHrs = 0;
+        System.out.printf("%5s     %5s     %5s     %5s\n", "Day", "Workinghrs", "Wage", "Total working hrs");
+        for (int day = 1, totalWorkingHrs = 0; day <= MAX_WORKING_DAYS && totalWorkingHrs < MAX_WORKING_HRS; day++, totalWorkingHrs += workingHrs)
         {
             int empType = (int) (Math.random() * 100) % 3;
-            int workingHours =0;
             switch (empType)
             {
                 case FULL_TIME:
-                    workingHours = 8;
+                    workingHrs = 8;
                     break;
                 case PART_TIME:
-                    workingHours = 4;
+                    workingHrs = 4;
                     break;
                 default:
+                    workingHrs = 0;
+                    break;
             }
-            int wage = workingHours * WAGE_PER_HR;
-            System.out.println("Day " + day + " wage is:" + wage);
+            int wage = workingHrs * WAGE_PER_HR;
             totalWage += wage;
+            System.out.printf("%5d       %5d      %5d      %5d\n", day, workingHrs, wage, totalWorkingHrs + workingHrs);
         }
         System.out.println("Total wage for a month is " + totalWage);
     }

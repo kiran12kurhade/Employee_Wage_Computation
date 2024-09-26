0b409e80b150779842a51529c44cc034396649ce
kiran12kuhrade <Github@1999>
2024-09-26 14:30:44 +0530

    Added-Welcome Message

---
+++ src/main/java/com/Employee/EmployeeWageComputation.java
@@ -0,0 +1,8 @@
+package com.Employee;
+
+public class EmployeeWageComputation {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Employee Wage Computtaion!!");
+    }
+}

6dd62c162517aee15652a3ca2aa783f1ad944ccc
kiran12kuhrade <Github@1999>
2024-09-26 15:00:04 +0530

    Added:Part Time and Full Time Wage

--- src/main/java/com/Employee/EmployeeWageComputation.java
+++ src/main/java/com/Employee/EmployeeWageComputation.java
@@ -5,16 +5,27 @@ public class EmployeeWageComputation {
 
         System.out.println("Welcome to Employee Wage Computtaion!!");
 
-        final int FULL_TIME = 1;
-        double empType = Math.floor(Math.random() * 10) % 2;
+        int FULL_TIME = 2;
+        int PART_TIME = 1;
+        int WAGE_PER_HR = 20;
+        int workingHours = 0;
+
+        double empType = Math.floor(Math.random() * 10) % 3;
         if (empType == FULL_TIME)
         {
-            System.out.println("Employee is Present");
+            System.out.println("Employee is Present in Full Time");
+            workingHours=8;
+
+        } else if (empType == PART_TIME) {
+            System.out.println("Employee is Present in Part Time" );
+            workingHours =4;
 
         } else
         {
             System.out.println("Employee is Absent");
 
         }
+        int wage = workingHours * WAGE_PER_HR;
+        System.out.println("Employee Daily Wage is " + wage);
     }
 }
